58abb98b16f665cf463d77b9cf96a013eba78830
Thomas Conner <thomas.conner@me.com>
2019-10-14 14:12:22 -0400

    Update config files
    - Add linting with ESLint
    - Use prettier to fix common formatting
    - Update tsconfig.json

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  plugins: [
+    '@typescript-eslint',
+  ]
+};
