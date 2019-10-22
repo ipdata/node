f4d678b31aa6d8dcc4b5bc7bf8d3433e59834b50
Thomas Conner <thomas.conner@me.com>
2019-10-22 08:26:15 -0400

    Fix eslint errors

--- src/ipdata.ts
+++ src/ipdata.ts
@@ -1,24 +1,25 @@
 import * as request from 'request-promise';
 import * as urljoin from 'url-join';
 
+// eslint-disable-next-line @typescript-eslint/interface-name-prefix
 export interface IPDataLookupResponse {
-  ip: string,
-  city: string,
-  region: string,
-  country_name: string,
-  country_code: string,
-  continent_name: string,
-  continent_code: string,
-  latitude: number,
-  longitude: number,
-  asn: string,
-  organisation: string,
-  postal: string,
-  currency: string,
-  currency_symbol: string,
-  calling_code: string,
-  flag: string,
-  time_zone: string
+  ip: string;
+  city: string;
+  region: string;
+  country_name: string;
+  country_code: string;
+  continent_name: string;
+  continent_code: string;
+  latitude: number;
+  longitude: number;
+  asn: string;
+  organisation: string;
+  postal: string;
+  currency: string;
+  currency_symbol: string;
+  calling_code: string;
+  flag: string;
+  time_zone: string;
 }
 
 export function lookup(ip?: string, apiKey?: string, language?: string): Promise<IPDataLookupResponse> {
@@ -40,6 +41,6 @@ export function lookup(ip?: string, apiKey?: string, language?: string): Promise
   return request({
     uri: uri,
     headers: headers,
-    json: true
+    json: true,
   });
 }
