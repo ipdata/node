8f62fbc58a67d7a10592d16d79419d8b4cfcb887
Thomas Conner <thomas.conner@me.com>
2019-11-03 19:39:08 -0500

    Add missing tests

--- src/ipdata.test.ts
+++ src/ipdata.test.ts
@@ -1,39 +1,60 @@
-import nock from 'nock';
 import IPData from './ipdata';
 
-const IP = '8.8.8.8';
+const API_KEY = 'test';
+
+describe('constructor()', () => {
+  it('should throw an error if an apiKey is not provided', () => {
+    // @ts-ignore
+    expect(() => new IPData()).toThrowError('An API key is required.');
+  });
+
+  it('should set the apiKey', () => {
+    const ipdata = new IPData(API_KEY);
+    expect(ipdata.apiKey).toEqual(API_KEY);
+  });
+
+  it('should turn on the cache by default', () => {
+    const ipdata = new IPData(API_KEY);
+    expect(ipdata.useCache).toEqual(true);
+  });
+
+  it('should turn off the cache', () => {
+    const ipdata = new IPData(API_KEY, false);
+    expect(ipdata.useCache).toEqual(false);
+  });
+})
 
 describe('lookup()', () => {
-  const ipdata = new IPData('test', false);
-
-  // afterEach(() => {
-  //   return clearCache(IP);
-  // });
+  const ipdata = new IPData(API_KEY, false);
+  const IP = '1.1.1.1';
 
   it('should throw an error if the ip is invalid', async () => {
-    const ip = '1.1.11';
-    await expect(ipdata.lookup(ip)).rejects.toThrowError(`${ip} is an invalid IP address.`);
+    const badIP = '1.1.11';
+    await expect(ipdata.lookup(badIP)).rejects.toThrowError(`${badIP} is an invalid IP address.`);
   });
 
   it('should return default information when no ip is provided', async () => {
     const info = await ipdata.lookup();
     expect(info).toHaveProperty('ip');
+    expect(info).toHaveProperty('status');
   });
 
   it('should return default information when null is provided', async () => {
     const info = await ipdata.lookup(null);
     expect(info).toHaveProperty('ip');
+    expect(info).toHaveProperty('status');
   });
 
   it('should return default information when undefined is provided', async () => {
     const info = await ipdata.lookup(undefined);
     expect(info).toHaveProperty('ip');
+    expect(info).toHaveProperty('status');
   });
 
   it('should set an ip in the uri', async () => {
-    const ip = '8.8.8.8';
-    const info = await ipdata.lookup(ip);
-    expect(info).toHaveProperty('ip', ip);
+    const info = await ipdata.lookup(IP);
+    expect(info).toHaveProperty('ip', IP);
+    expect(info).toHaveProperty('status');
   });
 
   it('should throw an error if a selectField and fields is provided', async () => {
@@ -49,29 +70,76 @@ describe('lookup()', () => {
     });
 
     it('should return a response with only the field', async () => {
-      const field = 'asn';
+      const field = 'is_eu';
       const info = await ipdata.lookup(null, field);
-      expect(info).toHaveProperty('select_field');
+      expect(info).toHaveProperty('select_field', false);
+      expect(info).toHaveProperty('status');
     });
   });
 
   describe('fields', () => {
+    it('should throw an error for an invlaid field ', async () => {
+      const field = 'field';
+      const fields = [field];
+      await expect(ipdata.lookup(null, null, fields)).rejects.toThrowError(`${field} is not a valid field.`);
+    });
 
+    it('should return a response with only the field', async () => {
+      const field1 = 'ip';
+      const field2 = 'is_eu';
+      const fields = [field1, field2];
+      const info = await ipdata.lookup(IP, null, fields);
+      expect(info).toHaveProperty(field1, IP);
+      expect(info).toHaveProperty(field2, false);
+      expect(info).toHaveProperty('status');
+    });
+  });
+});
+
+describe('lookupBulk()', () => {
+  const ipdata = new IPData(API_KEY, false);
+  const IP1 = '1.1.1.1';
+  const IP2 = '8.8.8.8';
+
+  it('should throw an error if less than 2 ips are provided', async () => {
+    const ips = [IP1];
+    await expect(ipdata.lookupBulk(ips)).rejects.toThrowError(
+      'Bulk Lookup requires more than 1 IP Address in the payload.',
+    );
   });
 
-  // it('should return the response from the cache', async () => {
-  //   await ipdata.lookup(IP);
-  //   nock.disableNetConnect();
-  //   const info = await ipdata.lookup(IP);
-  //   expect(info).toHaveProperty('ip', IP);
-  //   nock.enableNetConnect();
-  // });
+  it('should throw an error if an ip is invalid', async () => {
+    const badIP = '1.1.11';
+    const ips = [badIP, IP2];
+    await expect(ipdata.lookupBulk(ips)).rejects.toThrowError(`${badIP} is an invalid IP address.`);
+  });
 
-  // it('should call onResponse callback', async () => {
-  //   const ipdata = new IPData(API_KEY);
-  //   const onResponseSpy = jest.fn();
-  //   await ipdata.lookup(IP, { onResponse: onResponseSpy });
-  //   expect(onResponseSpy).toHaveBeenCalledTimes(1);
-  //   expect(onResponseSpy.mock.calls[0][0]).toHaveProperty('status', 200);
-  // });
+  it('should return info for the ips', async () => {
+    const info = await ipdata.lookupBulk([IP1, IP2]);
+    expect(info).toHaveProperty('responses');
+    expect(info).toHaveProperty('status');
+    expect(info.responses[0]).toHaveProperty('ip', IP1);
+    expect(info.responses[1]).toHaveProperty('ip', IP2);
+  });
+
+  describe('fields', () => {
+    it('should throw an error for an invlaid field ', async () => {
+      const field = 'field';
+      const fields = [field];
+      await expect(ipdata.lookupBulk([IP1, IP2], fields)).rejects.toThrowError(`${field} is not a valid field.`);
+    });
+
+    it('should return a response with only the field', async () => {
+      const field1 = 'ip';
+      const field2 = 'is_eu';
+      const fields = [field1, field2];
+      const info = await ipdata.lookupBulk([IP1, IP2], fields);
+      expect(info).toHaveProperty('responses');
+      expect(info).toHaveProperty('status');
+      expect(info.responses[0]).toHaveProperty(field1, IP1);
+      expect(info.responses[0]).toHaveProperty(field2, false);
+      expect(info.responses[1]).toHaveProperty(field1, IP2);
+      expect(info.responses[1]).toHaveProperty(field2, false);
+    });
+  });
 });
