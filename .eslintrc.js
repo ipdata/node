342fb0b8a3ce5d1ac0facbf32148c5d8f20708ab
Thomas Conner <thomas.conner@me.com>
2019-11-04 07:43:05 -0500

    fix: fix lint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,23 +4,23 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    node: true,
+    node: true
   },
   extends: [
     'airbnb-base',
     'plugin:@typescript-eslint/recommended',
     'prettier/@typescript-eslint',
-    'plugin:prettier/recommended',
+    'plugin:prettier/recommended'
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: path.resolve(__dirname, './tsconfig.json')
   },
   plugins: [
-    '@typescript-eslint',
+    '@typescript-eslint'
   ],
   rules: {
-    'lines-between-class-members': 'off',
+    'lines-between-class-members': 'off'
   },
   overrides: [
     {
@@ -28,9 +28,9 @@ module.exports = {
       rules: {
         // Import
         'import/no-extraneous-dependencies': 'off'
-      }
+      },
       env: {
-        jest: true,
+        jest: true
       }
     }
   ],
